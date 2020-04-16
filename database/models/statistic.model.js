fd1dd327b2f52ec1a024dc0d580c517f40855df7
Elvis <nm@vectra.co.za>
2020-04-16 19:13:06 +0200

    model changes

--- database/models/statistic.model.js
+++ database/models/statistic.model.js
@@ -1,13 +1,26 @@
 const mongoose = require('mongoose')
 const Types = mongoose.Schema.Types
 
-module.exports = mongoose.model('Statistic', new mongoose.Schema({
-  confirmedCases: Number,
-  testCases: Number,
-  dailyTestCases: Number,
-  deaths: Number,
-  unknownCases: Number,
-  date: Date,
-  country: String,
-  region: String
-}))
+module.exports = {
+  Statistic: mongoose.model('Statistic', new mongoose.Schema({
+    confirmedCases: Number,
+    testCases: Number,
+    dailyTestCases: Number,
+    deaths: Number,
+    unknownCases: Number,
+    date: Date,
+    country: String,
+    region: String
+  })),
+
+  StatisticSnapshot: mongoose.model('StatisticSnapshot', new mongoose.Schema({
+    confirmedCases: Number,
+    testCases: Number,
+    dailyTestCases: Number,
+    deaths: Number,
+    unknownCases: Number,
+    date: Date,
+    country: String,
+    region: String
+  }))
+}
